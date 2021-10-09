d371e73886b68be2448f04149779188a864e7af5
Benjamin Nielsen <56326234+ben-baqa@users.noreply.github.com>
2021-10-09 16:02:43 -0600

    Noisy Nodes setup, buoyancy volume visualization.

---
+++ Assets/Editor/BuoyancyEditor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Buoyancy))]
+public class BuoyancyEditor : Editor
+{
+    private Buoyancy b;
+
+    public void OnSceneGUI()
+    {
+        b = this.target as Buoyancy;
+        Handles.color = new Color(.5f, .3f, .35f, .8f);
+        Handles.matrix = b.transform.localToWorldMatrix;
+        for(int i = 0; i <= b.sections; i++)
+        {
+            DrawCrossSection((b.height * i) / b.sections, b.length,
+                b.crossSectionalArea * b.areaCurve.Evaluate(i / (float)b.sections));
+        }
+    }
+
+    private void DrawCrossSection(float height, float length, float area)
+    {
+        float squish = area / (Mathf.PI * length * length);
+        Matrix4x4 squishMatrix = Matrix4x4.Scale(new Vector3(squish, 1, 1));
+        Handles.matrix = squishMatrix * b.transform.localToWorldMatrix;
+        //Handles.matrix = Matrix4x4.TRS(b.transform.position, b.transform.rotation,
+        //    new Vector3(squish, 1, 1));
+        Handles.DrawWireDisc(Vector3.up * height, Vector3.up, length);
+        //Handles.matrix *= squishMatrix.inverse;
+    }
+}
--- Assets/Scripts/Buoyancy.cs
+++ Assets/Scripts/Buoyancy.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 // F = - (Object density - fluid density) (gravitational acceleration) (Displaced Volume)
 public class Buoyancy : MonoBehaviour
 {
-    /// density of the object in kg/m^3
+    [Header("Physical Attributes")]
+    [Tooltip("density of the object in kg/m^3")]
     public float density = .2f;
-    /// maximum cross sectional area
-    public float crossSectionalArea = 1;
-    /// Curve representing change in area over height
+    [Tooltip("maximum cross sectional area in m^2")]
+    public float crossSectionalArea = 3;
+    [Tooltip("Curve representing change in area over height")]
     public AnimationCurve areaCurve;
-    /// height of the object
-    public float height
+    [Tooltip("height of the object in m")]
+    public float height;
+
+    [Header("Debug display only")]
+    public float length = 5;
+    public int sections = 5;
+
+    //private void OnDrawGizmosSelected()
+    //{
+    //    Gizmos.matrix = transform.localToWorldMatrix;
+    //    for(int i = 0; i < sections; i++)
+    //    {
+    //        Gizmos.dr
+    //    }
+    //}
 
     // Start is called before the first frame update
     void Start()
